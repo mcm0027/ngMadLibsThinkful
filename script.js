012ce9461e8b4e92dbbfaa5dee9ce5be5244282e
Matt Morris <mcm0027@yahoo.com>
2015-11-04 12:39:58 -0600

    update script.js and add gender specific pronouns

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-angular.module('myApp', []);
+angular.module('myApp', [])
+  .controller('oneController', ['$scope', function($scope) {
+  $scope.sexMain = {
+    name: 'female'
+  };
+    
+  $scope.sexCeleb = {
+    name: 'female'
+  };
+    
+  $scope.sexBadCeleb = {
+    name: 'female'
+  }  
+  }]);
 
   
