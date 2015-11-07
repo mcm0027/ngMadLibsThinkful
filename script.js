fb99ca72a723b3cd84d4a7f4bcd9a0fa6f494b94
Matt Morris <mcm0027@yahoo.com>
2015-11-06 21:18:42 -0600

    add error message and form validation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngMessages'])
   .controller('oneController', ['$scope', function($scope) {
   $scope.sexMain = {
     name: 'female'
@@ -10,7 +10,49 @@ angular.module('myApp', [])
     
   $scope.sexBadCeleb = {
     name: 'female'
-  }  
-  }]);
-
+  }
   
+  $scope.submit = function() {
+    if ($scope.madForm.$valid) {
+      console.log("the form is valid")
+    } else {
+      $(".popup").show();
+      $scope.madForm.$submitted = false;
+    }
+  }
+  
+  $scope.reset = function() {
+    document.getElementById("form").reset();
+    $scope.madForm.$submitted = false;
+    $scope.madForm.$valid = false;
+ }
+    
+}]);
+
+
+setTimeout(function(){
+  $(".popup").hide();}, 1);
+  
+  
+$(document).click(function(){
+		$(".popup").hide();
+	});
+
+	
+$(document).keydown(function(){ 
+		$(".popup").hide();
+	});
+  
+
+
+setTimeout(function(){
+  $("#she-main").attr('checked','checked');
+},10);
+
+setTimeout(function(){
+  $("#she-celeb").attr('checked','checked');
+},10);
+
+setTimeout(function(){
+  $("#she-bad").attr('checked','checked');
+},10);
